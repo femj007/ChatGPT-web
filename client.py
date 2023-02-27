f51321da61dd913e06f18325f6eb452db4145569
Dong JinGuo <xydjg@qq.com>
2023-02-28 02:24:39 +0800

    Add revChatGPT

---
+++ client.py
@@ -0,0 +1,16 @@
+from revChatGPT.V1 import Chatbot
+
+chatbot = Chatbot(config={
+  "email": os.getenv("OPENAI_EMAIL"),
+  "password": os.getenv("OPENAI_PASSWORD")
+})
+
+print("Chatbot: ")
+prev_text = ""
+for data in chatbot.ask(
+    "Hello world",
+):
+    message = data["message"][len(prev_text) :]
+    print(message, end="", flush=True)
+    prev_text = data["message"]
+print()
